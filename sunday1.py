1f45584890492dd4ef4ac452f867b328e5b1af31
Sophia Sommers <soommers.sophia@gmail.com>
2023-08-13 19:18:01 -0400

    learning new things!

---
+++ sunday1.py
@@ -0,0 +1,23 @@
+# nonstandard problem - a quine
+# write a program that prints its own source code
+#
+# the execution of the program should return a string 
+# with the source code
+
+#ex. the eraborus quine
+
+# def prints_itself():
+#     #If I want to print the source code, I need to make it printable first
+
+#     #The more characters I have, the more I have to handle
+#     # (especially special characters)
+#     wholefile = compile(every_line_in_file)
+#     for char in wholefile:
+#         print(char)
+
+# ######
+
+# string_obj = "def print_itself():";"print()"
+# def print_itself():
+#     print()
+
